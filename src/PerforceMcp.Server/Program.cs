47b3b91b5553f05cbd22c5ba9bd6f3e493fe2cc8
frossdelaqrois <frossdelaqrois@gmail.com>
2026-07-13 01:21:18 +0100

    Fix MCP stdio startup logging

--- src/PerforceMcp.Server/Program.cs
+++ src/PerforceMcp.Server/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PerforceMcp.Perforce;
+using PerforceMcp.Server;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+StdioLogging.Configure(builder.Logging);
 P4ExecutableDiscoveryResult executable = await new P4ExecutableDiscovery().DiscoverAsync(
     new P4ExecutableDiscoveryOptions
     {
